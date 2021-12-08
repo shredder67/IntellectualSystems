e4678d13535bcb267a32f1eb7555df8064f9c3fb
shredder67 <seriy.karp2@gmail.com>
2021-12-08 16:33:18 +0300

    Modified T-funtion and added ploting

--- SimulatedAnnealing/main.py
+++ SimulatedAnnealing/main.py
@@ -1,7 +1,7 @@
 # Задача о назначениях https://en.wikipedia.org/wiki/Assignment_problem
 # Датасет взят здесь http://people.brunel.ac.uk/~mastjjb/jeb/orlib/assigninfo.html
 
-
+from matplotlib import pyplot as plt
 import pandas as pd
 from math import exp, log
 from random import random, randint, shuffle
@@ -13,24 +13,14 @@ import data_service as ds
 def E(assignments, costs) -> float:
     value = 0
     for i in range(len(assignments)):
-        value += costs[assignments[i]][i]
+        value += costs[i][assignments[i]]
     return value
 
 
 # Функция понижения температуры
 def T(t):
-    k = 0.5
-    return t / (1 + k * t)
-
-
-# Функция генерации следующего состояния системы
-def G(x, t):
-    i = randint(0, len(x) - 1)
-    j = randint(0, len(x) - 1)
-
-    new_x = x.copy()
-    new_x[i], new_x[j] = new_x[j], new_x[i]
-    return new_x
+    k = 0.99
+    return k * t
 
 
 # Функция вероятности перехода к следующему состоянию
@@ -44,38 +34,53 @@ def check_prob(v) -> bool:
     return random() < v
 
 
-def main():
+def anneal():
     path, ans = ds.get_parsed_args()
     costs = pd.DataFrame.from_records([row for row in ds.read_data_by_rows(path)])
 
-    # i-тый столбец содержит стоимость назначения i-того водителя ко всем пассажирам
+    # i-тая строка содержит стоимость назначения i-того водителя ко всем пассажирам
 
-    cur_solution = list(range(len(costs[0])))  # i - пассажир, solution[i] - водитель
+    cur_solution = list(range(len(costs[0])))  # s[i] = j : i-тый водитель j-тому пассажиру
     shuffle(cur_solution)
     f = E(cur_solution, costs)
 
     f_global_min = f
-    temp = 100_000_000
-    i = 1
+    max_temp = 10_000
+    temp = max_temp
+    k = 1
+    y_values = []
     while temp > 0.01:
         if f_global_min > f:
             f_global_min = f
-            print(f_global_min) 
 
-        new_solution = G(cur_solution, temp)
-        f_new = E(new_solution, costs)
 
-        if check_prob(h(f_new - f, temp)):
-            cur_solution = new_solution
-            f = f_new
+        while True:
+            i = randint(0, len(cur_solution) - 1)
+            j = randint(0, len(cur_solution) - 1)
+            cur_solution[i], cur_solution[j] = cur_solution[j], cur_solution[i]
+            cur_solution[i:j + 1]
+            f_new = E(cur_solution, costs)
+            prob = h(f_new - f, temp)
 
-        if i % 15 == 0:
-            temp = T(temp)
-        i += 1
+            if check_prob(prob):
+                f = f_new
+                break
+            else:
+                cur_solution[i], cur_solution[j] = cur_solution[j], cur_solution[i]
+
+        y_values.append(f)
+        temp = T(temp)
+        k += 1
 
     print(f_global_min)
     if ans is not None:
         print("Правильный ответ:", ans)
+
+    return list(range(k - 1)), y_values
+
+def main():
+    plt.plot(anneal())
+    plt.show()
 
 
 if __name__ == '__main__':
